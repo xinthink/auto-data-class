879adc2330452c5e74b1eaf2d4e6089a114a607a
Think Wu <yingxinwu.g@gmail.com>
2017-11-20 09:32:29 +0800

    fix problem with Java primitive types

--- example/src/test/java/com/fivemiles/auto/dataclass/PropTypesTest.kt
+++ example/src/test/java/com/fivemiles/auto/dataclass/PropTypesTest.kt
@@ -21,6 +21,24 @@ package com.fivemiles.auto.dataclass
     val ls: List<String>
 //    val lns: List<String?>  // nullable type argument unsupported
     val msi: Map<String, Int>
+    val msl: Map<String, Long>
+    val mss: Map<String, Short>
+    val msb: Map<String, Byte>
+    val msf: Map<String, Float>
+    val msd: Map<String, Double>
+    val msz: Map<String, Boolean>
     val ss: Set<String>
+    val sl: Set<Long>
+    val ssh: Set<Short>
+    val sb: Set<Byte>
+    val sf: Set<Float>
+    val sd: Set<Double>
+    val sz: Set<Boolean>
     val lmsi: List<Map<String, Int>>
+    val lmsl: List<Map<String, Long>>
+    val lmss: List<Map<String, Short>>
+    val lmsb: List<Map<String, Byte>>
+    val lmsf: List<Map<String, Float>>
+    val lmsd: List<Map<String, Double>>
+    val lmsz: List<Map<String, Boolean>>
 }
